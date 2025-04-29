5f83997051a794d5eb70b10af64d75dbb9eb17b9
brandon <brandon>
2025-04-29 09:50:18 +0200

    fix: changed class name for utils_uxi

--- pyhpeuxi/utils_uxi.py
+++ pyhpeuxi/utils_uxi.py
@@ -5,10 +5,10 @@ import json
 import csv
 import os
 
-class Utils_UXI():
+class UtilsUXI():
     def get_token_from_file(fileName):
         """
-        Use this function to retrieve the access_token from a file which is located in the working directory of the script. 
+        Use this function to retrieve the access_token from a file which is located in the working directory of the script.
         The file must use the following structure:
         {"access_token":"your_secret_token"}
         """
@@ -20,20 +20,20 @@ class Utils_UXI():
                 return jsonContent['access_token']
             else:
                 raise ValueError({"status":"Error", "message":"Missing Access Token."})
-     
+
         except KeyError as e:
             raise Exception({"status":"Error", "Message":"Missing 'access_token' key name. Ensure key name exists as per the function usage details."})
         except json.JSONDecodeError as e:
             raise Exception({"status":"Error", "Message":"Ensure a valid json file as described in the function usage details."})
-        except FileNotFoundError as e:  
+        except FileNotFoundError as e:
             raise Exception({"status":"Error", "Message":"Ensure the token file '"+ fileName+"' exists. "+e.strerror})
 
         except Exception as e:
             raise Exception({"status":"Error", "Message":e})
-    
+
     def get_personal_api_client_creds_from_file(fileName):
         """
-        Use this function to retrieve the client_id and client_secret from a file which is located in the working directory of the script. 
+        Use this function to retrieve the client_id and client_secret from a file which is located in the working directory of the script.
         The file must use the following structure:
         {"client_id":"your_client_id","client_secret":"your_client_secret"}
         """
@@ -44,22 +44,22 @@ class Utils_UXI():
             if jsonContent['client_id'] and jsonContent['client_secret']:
                 return jsonContent
             else:
-                raise ValueError({"status":"Error", "message":"Ensure both client_id and client_secret key names and values exist within the ."})   
-        
+                raise ValueError({"status":"Error", "message":"Ensure both client_id and client_secret key names and values exist within the ."})
+
         except KeyError as e:
             raise Exception({"status":"Error", "Message":"Missing 'client_id' key name or and client_secret. Ensure key name exists as per the function usage details."})
         except json.JSONDecodeError as e:
             raise Exception({"status":"Error", "Message":"Ensure a valid json file as described in the function usage details."})
-        except FileNotFoundError as e:  
+        except FileNotFoundError as e:
             raise Exception({"status":"Error", "Message":"Ensure the token file '"+ fileName+"' exists. "+e.strerror})
 
         except Exception as e:
-            raise Exception({"status":"Error", "Message":e})     
+            raise Exception({"status":"Error", "Message":e})
 
     def export_to_csv(data, file_name, sub_folder="csv"):
         """
         Exports the data to a CSV file. Parameters are described below
-        
+
         :param The data to be exported (expected to be a dictionary with an 'items' key).
         :param file_name: The name of the CSV file to create without the extension.
         :param sub_folder: The subfolder where the CSV file will be saved. #Optional, by default will create a folder called 'csv'
